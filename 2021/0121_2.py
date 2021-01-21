5e83c2a902d1d7582f4e5198f6ecc1777aabcf84
G2 <left.left2222@gmail.com>
2021-01-21 23:06:57 +0900

    study python

---
+++ 2021/0121_2.py
@@ -0,0 +1,14 @@
+tpl = ("MZ", 700, "FM", "7")
+print tpl
+print tpl[1]
+print tpl[1:3]
+print len(tpl)
+tpl1 = ("SORD", "M5", "Apple", "II")
+tpl2 = tpl + tpl1
+print tpl2
+tpl3 = tpl*3
+print tpl3
+flag = 700 in tpl
+print flag
+flag = 1200 in tpl
+print flag
